b5bbc14fd6935d497f3266d5be9bdf471111d223
Firooz-shahriari <firoozshahriari@gmail.com>
2021-02-23 21:47:36 +0100

    update utilities.py file
    As the 'monitor' function only prints not return, we defined another 
    similar function to be able to wrtite the test.

--- utilities.py
+++ utilities.py
@@ -1,7 +1,11 @@
 """
 This module is implemented to inform us about the progress of the algorithms,
-during the experimets. 
+during the experimets.
 """
 def monitor(name,current,total):
     if (current+1) % (total/10) == 0:
         print ( name + ' %d%% completed' % int(100*(current+1)/total) )
+
+def monitor_testing(name,current,total):
+    if (current+1) % (total/10) == 0:
+        return f'{name} {int(100*(current+1)/total)}% completed'

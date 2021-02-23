acc327c17411f086d3750b0901de41590d9ce0cb
Firooz-shahriari <firoozshahriari@gmail.com>
2021-02-23 15:58:29 +0100

    Add Utilities.py file.
    at the moment, only one function that monitor the progress of the 
    algorithm.

---
+++ utilities.py
@@ -0,0 +1,7 @@
+"""
+This module is implemented to inform us about the progress of the algorithms,
+during the experimets. 
+"""
+def monitor(name,current,total):
+    if (current+1) % (total/10) == 0:
+        print ( name + ' %d%% completed' % int(100*(current+1)/total) )

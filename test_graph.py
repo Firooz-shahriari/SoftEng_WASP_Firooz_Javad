cee9e0a749d622f0ccfff17f30ec2672f19620dc
Firooz-shahriari <firoozshahriari@gmail.com>
2021-02-24 01:44:03 +0100

    Update test_graph.py
    Add some test for Exponential Grpah, whether directed or undirected

--- test_graph.py
+++ test_graph.py
@@ -40,5 +40,26 @@ class test_graph(unittest.TestCase):
         self.assertGreaterEqual(np.count_nonzero(self.D), np.count_nonzero(self.C))
 
 
+    """ As the Expo graph is structured, test whther some samples must be  checked"""
+    def test_Expo_directed_1(self):
+        A = Exponential(3).directed()
+        B = Exponential(4).directed()
+        True_A = [[0., 1., 1.], [1., 0., 1.], [1., 1., 0.]]
+        True_B = [[0., 1., 1., 0.], [0., 0., 1., 1.], [1., 0., 0., 1.], [1., 1., 0., 0.]]
+        self.assertTrue((A == True_A).all())
+        self.assertTrue((B == True_B).all())
+
+    """ If it is directed, it should be stronly connected"""
+    def test_Expo_directed_2(self):
+        C = Exponential(10).directed()
+        CC = nx.from_numpy_matrix(C, create_using=nx.DiGraph)
+        lenC = len(max(nx.kosaraju_strongly_connected_components(CC), key=len))
+        self.assertEqual(lenC, C.shape[0])
+
+    """ As the graph is undirected its adjacency should be symmetric """
+    def test_Expo_undirected_1(self):
+        E = Exponential(10).undirected()
+        self.assertTrue( (E == np.transpose(E)).all())
+
 if __name__ == '__main__':
     unittest.main()
